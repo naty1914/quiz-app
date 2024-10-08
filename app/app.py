6bb61827ca50b9e121170bd786df5488d7e3337c
Natnael Seifu <natiseifu02@gmail.com>
2024-10-08 19:45:37 -0100

    Create a simple registration and login forms with Bootsrap

--- app/app.py
+++ app/app.py
@@ -6,6 +6,13 @@ app = Blueprint('app', __name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/login')
+def login():
+    return render_template('login.html')
+@app.route('/register')
+def register():
+    return render_template('registration.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
