00d418e38f091eedabd1e7eec8517d40a6501226
Natnael Seifu <natiseifu02@gmail.com>
2024-10-08 22:46:16 -0100

    Implement user authentication with registration, login and create user dashboard page, add logout functionality in dashboard

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,30 @@
 from flask import Flask
+from flask_login import LoginManager
+from app.database import db
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+from app.models import User
+login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
     
+    app.config['SECRET_KEY'] =os.environ.get('SECRET_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+   
+    db.init_app(app)
+    login_manager.init_app(app)
+    
+    login_manager.login_view = 'app.login'  
     from .app import app as app_blueprint
     app.register_blueprint(app_blueprint)
     
     return app
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
--- app/app.py
+++ app/app.py
@@ -1,18 +1,66 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template, redirect, url_for, flash, request
+from flask_login import current_user, login_user, logout_user, login_required
+from app import db
+from app.models import User
 
 app = Blueprint('app', __name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
-@app.route('/login')
-def login():
-    return render_template('login.html')
-@app.route('/register')
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        username = request.form.get('name')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        print(f"Username: {username}, Email: {email}, Password: {password}")
+        
+        existing_user = User.query.filter_by(email=email).first()
+        if existing_user:
+            return render_template('registration.html', error='Email  already exists. Please try again.')
+        existing_username = User.query.filter_by(username=username).first()
+        if existing_username:
+           error_message = 'Username already exists. Please try again.'
+           return render_template('registration.html', error=error_message) 
+        
+        new_user = User(username=username, email=email)
+        new_user.set_password(password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('app.login'))
+
     return render_template('registration.html')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        
+        user = User.query.filter_by(email=email).first()
+      
+        if user and user.check_password(password):
+            login_user(user)
+            return redirect(url_for('app.dashboard'))
+        else:
+            return render_template('login.html', error='Login failed. Please check your email and password.')
+    
+    return render_template('login.html')  
+
+
+@app.route('/dashboard')
+@login_required 
+def dashboard():
+    return render_template('dashboard.html', user=current_user)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return render_template('login.html', message='Logged out successfully!', message_class='success')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    from app import create_app
+    app_instance = create_app()
+    app_instance.run(debug=True)
