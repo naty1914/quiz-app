31ae4735f5add40ed66ce3e508f55db9197836fb
Natnael Seifu <natiseifu02@gmail.com>
2024-10-10 15:41:02 -0100

    Add countdown timer to quiz page with auto-submission when time expires

---
+++ app/static/script.js
@@ -0,0 +1,19 @@
+let timeLeft = 60;
+let time_id = setInterval(countdown, 1000)
+
+function countdown() {
+  if (timeLeft == 0) {
+    clearInterval(time_id);
+    document.getElementById('timer').textContent = 'Time is up';
+    setTimeout(function() {
+      document.getElementById("quizForm").submit();
+    }, 2000);
+  
+  } else {
+    document.getElementById('timer').textContent = `${timeLeft} Seconds remaining`;
+    timeLeft --;
+  }
+
+}
+
+countdown();
